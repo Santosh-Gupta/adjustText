c3bb29c401e52d829651803aac2c61fdffc5918d
Santosh Gupta <santosh@iwinks.org>
2018-07-10 16:35:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='adjustText',
       description='Iteratively adjust text position in matplotlib plots to minimize overlaps',
       author='Ilya Flyamer',
       author_email='flyamer@gmail.com',
-      url='https://github.com/Santosh-Gupta/adjustText',
+      url='https://github.com/Phlya/adjustText',
       packages=['adjustText'],
       install_requires=['numpy', 'matplotlib']
      )
