5f8500de0184ad743c6ea0cae897454d2b2d61f5
Santosh Gupta <santosh@iwinks.org>
2018-07-10 17:10:20 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -557,6 +557,10 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
             plt.savefig('%s%s.%s' % (save_prefix,
                         '{0:03}'.format(i+1), save_format),
                         format=save_format, dpi=150)
+            print('test)
+            uploadModel = drive.CreateFile()
+            uploadModel.SetContentFile('%s%s.%s' % (save_prefix, '{0:03}'.format(i+1), save_format))
+            uploadModel.Upload()
         elif on_basemap:
             ax.draw(r)
         # Stop if we've reached the precision threshold, or if the x and y displacement
@@ -584,9 +588,6 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
                         '{0:03}'.format(i+1), save_format),
                         format=save_format, dpi=150)
             print('test')
-            uploadModel = drive.CreateFile()
-            uploadModel.SetContentFile('%s%s.%s' % (save_prefix, '{0:03}'.format(i+1), save_format))
-            uploadModel.Upload()
     elif on_basemap:
         ax.draw(r)
 
