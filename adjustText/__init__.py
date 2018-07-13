59f322222671f9cec37797395dbe5a00ceeebb8f
Santosh Gupta <santosh@iwinks.org>
2018-07-13 12:20:20 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -127,7 +127,6 @@ def optimally_align_text(x, y, texts, expand=(1., 1.), add_bboxes=[],
         va = ['bottom', 'top', 'center']
     alignment = list(product(ha, va))
 #    coords = np.array(zip(x, y))
-    print("part1") ###
     for i, text in enumerate(texts):
 #        tcoords = np.array(text.get_position()).T
 #        nonself_coords = coords[~np.all(coords==tcoords, axis=1)]
@@ -153,7 +152,6 @@ def optimally_align_text(x, y, texts, expand=(1., 1.), add_bboxes=[],
             else:
                 axout = 0
             counts.append((axout, c, intersections))
-        print("part2") ###
         # Most important: prefer alignments that keep the text inside the axes.
         # If tied, take the alignments that minimize the number of x, y points
         # contained inside the text.
@@ -163,11 +161,10 @@ def optimally_align_text(x, y, texts, expand=(1., 1.), add_bboxes=[],
         if 'x' in direction:
             text.set_ha(alignment[a][0])
         if 'y' in direction:
-            text.set_va(alignment[a][1])
-        print("part3") ###    
+            text.set_va(alignment[a][1])   
         bboxes[i] = text.get_window_extent(r).expanded(*expand).\
                                        transformed(ax.transData.inverted())
-        print("part4") ###
+        print("part ", i) ###
     return texts
 
 def repel_text(texts, renderer=None, ax=None, expand=(1.2, 1.2),
