050f528c423dc72eecc08883f2b1a7beff9cb867
Santosh Gupta <santosh@iwinks.org>
2018-07-12 17:29:46 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -5,6 +5,17 @@ from itertools import product
 import numpy as np
 from operator import itemgetter
 
+from google.colab import files
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
+
 if sys.version_info >= (3, 0):
     xrange = range
 
