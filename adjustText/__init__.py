5a53168c7088835b6514e1864d69a1666a518161
Santosh Gupta <Santosh.gupta.eng@gmail.com>
2018-07-12 18:19:02 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -567,10 +567,6 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
             plt.savefig('%s%s.%s' % (save_prefix,
                         '{0:03}'.format(i+1), save_format),
                         format=save_format, dpi=150)
-            print('step ', i)
-            uploadModel = drive.CreateFile()
-            uploadModel.SetContentFile('%s%s.%s' % (save_prefix, '{0:03}'.format(i+1), save_format))
-            uploadModel.Upload()
             
         elif on_basemap:
             ax.draw(r)
@@ -598,6 +594,10 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
             plt.savefig('%s%s.%s' % (save_prefix,
                         '{0:03}'.format(i+1), save_format),
                         format=save_format, dpi=150)
+            print('step ', i)
+            uploadModel = drive.CreateFile()
+            uploadModel.SetContentFile('%s%s.%s' % (save_prefix, '{0:03}'.format(i+1), save_format))
+            uploadModel.Upload()
     elif on_basemap:
         ax.draw(r)
 
