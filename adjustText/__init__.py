52e4902ce82c71d4f88f53b0bf9bb1d952b5ea38
Santosh Gupta <santosh@iwinks.org>
2018-07-29 10:20:58 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -525,11 +525,6 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
                                 xytext=get_midpoint(bbox),
                                 arrowprops=ap,
                                 *args, **kwargs)
-            
-        uploadModel = drive.CreateFile()
-        uploadModel.SetContentFile('%s%s.%s' % (save_prefix, '000b' , save_format))
-        uploadModel.Upload()
-        os.remove('%s%s.%s' % (save_prefix, '000b' , save_format))
         
     elif on_basemap:
         ax.draw(r)
