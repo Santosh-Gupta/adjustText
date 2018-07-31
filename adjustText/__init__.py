96673ae12a04622226fb3a38cc6b18d63742c261
Santosh Gupta <santosh@iwinks.org>
2018-07-30 20:53:27 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -580,8 +580,8 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
             bboxes = get_bboxes(texts, r, (1, 1), ax)
             kwap = kwargs.pop('arrowprops')
             for j, (bbox, text) in enumerate(zip(bboxes, texts)):
-                #ap = {'patchA':text} # Ensure arrow is clipped by the text
-                ap = {'patchA':None} # Ensure arrow is clipped by the text
+                ap = {'patchA':text} # Ensure arrow is clipped by the text
+                #ap = {'patchA':None} # Ensure arrow is clipped by the text
                 ap.update(kwap) # Add arrowprops from kwargs
                 ax.annotate("", # Add an arrow from the text to the point
                         xy = (orig_xy[j]),
@@ -633,8 +633,8 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
         bboxes = get_bboxes(texts, r, (1, 1), ax)
         kwap = kwargs.pop('arrowprops')
         for j, (bbox, text) in enumerate(zip(bboxes, texts)):
-            #ap = {'patchA':text} # Ensure arrow is clipped by the text
-            ap = {'patchA':None} # Ensure arrow is clipped by the text
+            ap = {'patchA':text} # Ensure arrow is clipped by the text
+            #ap = {'patchA':None} # Ensure arrow is clipped by the text
             ap.update(kwap) # Add arrowprops from kwargs
             ax.annotate("", # Add an arrow from the text to the point
                         xy = (orig_xy[j]),
