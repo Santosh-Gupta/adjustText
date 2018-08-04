2befd644b46388c966409972795b2c886883be2a
Santosh Gupta <santosh@iwinks.org>
2018-08-04 16:17:17 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -633,14 +633,15 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
                 gauth.credentials = GoogleCredentials.get_application_default()
                 drive = GoogleDrive(gauth)
             os.remove('%s%s.%s' % (save_prefix, '{0:03}'.format(i+1), save_format))
-            drawArrow.remove() #removed arrows otherwise old arrows remain
-            ax.texts.remove(drawArrow) #removed arrows otherwise old arrows remain
+#             drawArrow.remove() #removed arrows otherwise old arrows remain
+#             ax.texts.remove(drawArrow) #removed arrows otherwise old arrows remain
         
         elif on_basemap:
             ax.draw(r)
         
         drawArrow.remove() #removed arrows otherwise old arrows remain
         ax.texts.remove(drawArrow) #removed arrows otherwise old arrows remain
+        ax.draw()
         
         # Stop if we've reached the precision threshold, or if the x and y displacement
         # are both greater than the max over the last 10 iterations (suggesting a
