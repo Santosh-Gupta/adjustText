911c4ba1fc967847b99cbcf7de515b5d3e2f8c3b
Santosh Gupta <santosh@iwinks.org>
2018-08-04 18:28:28 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -642,6 +642,7 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
         drawArrow.remove() #removed arrows otherwise old arrows remain
         #ax.texts.remove(drawArrow) #removed arrows otherwise old arrows remain
         plt.draw()
+        ax.patches = []
         
         # Stop if we've reached the precision threshold, or if the x and y displacement
         # are both greater than the max over the last 10 iterations (suggesting a
