3d54646ba02179f27f1ba8dd3ed1768adf26290c
Santosh Gupta <santosh@iwinks.org>
2018-08-04 20:09:43 -0700

    Update __init__.py

--- adjustText/__init__.py
+++ adjustText/__init__.py
@@ -644,6 +644,7 @@ def adjust_text(texts, x=None, y=None, add_objects=None, ax=None,
         #ax.annotate = []
         plt.draw()
         plt.canvas.draw()
+        print("going here ", i)
         
         # Stop if we've reached the precision threshold, or if the x and y displacement
         # are both greater than the max over the last 10 iterations (suggesting a
